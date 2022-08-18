ed2f36c77a5c3d5fda889705adc429a343af1aa0
Lukasz <bleyosss@wp.pl>
2022-08-18 20:40:51 +0200

    css added

---
+++ app.js
@@ -0,0 +1,17 @@
+async function getRandomMeal(){
+    const request = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`);
+    const responseRandomMeal = await request.json();
+    console.log(responseRandomMeal);
+}
+getRandomMeal();
+
+async function getMealByName(){
+    const request = await fetch("www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata");
+    const responseRandomMeal = await request.json();
+    console.log(responseRandomMeal)
+}
+async function getMealById(){
+    const request = await fetch("www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata");
+    const responseRandomMeal = await request.json();
+    console.log(responseRandomMeal)
+}
