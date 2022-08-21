a42383ab28d22ac00ac2c9942d4c65c49ae9ba1f
Lukasz <bleyosss@wp.pl>
2022-08-21 12:43:44 +0200

    improved html structure and added some search mechanics

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const getMagnifierIcon = document.querySelector(".navigation__search--icon");
 const nav = document.querySelector(".navigation__search");
 const containerRandomRecipe = document.querySelector(".containerRand");
+const searchBox = document.querySelector(".navigation__search--input");
 
 document.addEventListener("DOMContentLoaded", () => {
     addEventListeners();
 })
 const addEventListeners = function(){
-    getMagnifierIcon.addEventListener("click", handleMagnifierClick)
+    getMagnifierIcon.addEventListener("click", handleMagnifierClick);
+    searchBox.addEventListener("input", searchLetterWrote)
 }
 const handleMagnifierClick = function() {
     nav.classList.toggle("navigation__search--active")
@@ -20,14 +22,15 @@ const getRandomMeal = async function(){
 }
 getRandomMeal();
 
-const getMealByName = async function(){
-    const request = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata");
+const getMealByName = async function(mealName){
+    const request = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=%${mealName}%`);
     const responseMealByName = await request.json();
     const mealByNameItem = await responseMealByName.meals;
+    console.log(mealByNameItem)
     return mealByNameItem;
 }
 const getMealById = async function(){
-    const request = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata");
+    const request = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772");
     const responseMealById = await request.json();
     const mealByIdItem = await responseMealById.meals;
     return mealByIdItem;
@@ -41,17 +44,18 @@ const startAsyncFunc = async function(){
     const mealById = await getMealById();
     addRandomDivToHtml(randomMeal);
 }
+startAsyncFunc();
 const createArticleComponent = function(strMeal, strMealThumb){
     return `
-                <h1 class="containerRand__randRecipe--name">${strMeal}</h1>
+                <h1 class="container__recipe--name">${strMeal}</h1>
 
-                <div class="containerRand__randRecipe--img">
+                <div class="container__recipe--img">
                     <img src="${strMealThumb}" alt="">
                 </div>
 
-                <div class="btnBox">
-                    <button class="btnBox__favBtn">Add to fav</button>
-                    <button class="btnBox__showRecipe"></button>
+                <div class="buttons">
+                    <button class="buttons__fav">Add to fav</button>
+                    <button class="buttons__show"></button>
                 </div>
             `
 }
@@ -59,8 +63,13 @@ const addRandomDivToHtml = function(randomMeal){
     const {strMeal, strMealThumb} = randomMeal;
     console.log(strMeal, strMealThumb);
     const randomMealSection = document.createElement("article");
-    randomMealSection.classList.add("containerRand__randRecipe");
+    randomMealSection.classList.add("container__recipe");
     randomMealSection.innerHTML = createArticleComponent(strMeal, strMealThumb);
     containerRandomRecipe.append(randomMealSection);
 }
-startAsyncFunc();
+const searchLetterWrote = function(){
+    setTimeout(() => {
+        const name = searchBox.value;
+        getMealByName(name)
+    }, 300);
+}
